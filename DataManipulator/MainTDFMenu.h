0bbe43fce21d9a0391df54f134f48927192c8aeb
Ilja Aboimov <ilja.ist.am.besten@mail.ru>
2024-12-26 16:47:46 +0300

    Data manipulator (#25)
    * visualization with infographic V-1.0
    
    * подготовка для разделения
    
    * перенёс файлы визуализации
    
    * мглавное меню для работы с данными и чтение данных
    
    * добавлено: обрезка данных по проценту и по количеству, выравнивание диапазонов по времени, сохранение данных
    
    * улучшения по правилам Чистого кода, а также завершение работы над обработкой OCDF данных
    
    * перенос некоторых функций в файл WARNING, универсальность функции чтения данных
    
    * визуализация данных одного сида и некоторые решения по чистому коду
    
    * визуализация OCDF формата
    
    * Добавление OCDF данных к уже имеющимся
    
    * удалил временные файлы
    
    * добавил возможность сохранение и четние данных из/в бинарные файлы
    
    * собрал табличный формат данных
    
    * Вынес наиболее общие файлы для работы с данными в папку Common
    
    * раздил главный файл на подфайлы для удобства
    
    * Составлено меню для работы с табличным форматом данных
    
    * Урезание данных для табличного формата и парсинг табличного формата по сиду
    
    * визуализация табличного формата
    
    * работа над обработчиком данных завершена
    
    * более информативные графики
    
    * кодировка файлов переведена в utf-8
    
    * исправлены ошибки сборки
    
    * исправлена поломка вывода символов utf8 в c++
    
    * удалил ненужные функции и заменил их на одну общую
    
    * адаптация для linux
    
    * readme: описание модуля
    
    * дополнение документации
    
    * дополненая документация (обрезка, визуализация )
    
    * исправлен вывод информации об ошибках
    
    * завершил документацию по модулю
    
    * начата документация в latex
    
    * новая функция обрезки по заданному моменту времени
    
    * вынес документацию для отдельного пул реквеста
    
    * Добавлена обработка ошибок при сохранении OCDF
    
    * Исправлены небольшие ошибки в тексте ошибок
    
    * Более информативная визуализация TDF

---
+++ DataManipulator/MainTDFMenu.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <functional>
+#include <filesystem>
+#include "MainOCDFMenu.h"
+#include "../Common/DataReformer.h"
+#include "../Common/DataReader.h"
+#include "../Common/DataDumper.h"
+
+using std::map;
+using std::function;
+using std::vector;
+using std::make_pair;
+
+void MainTDF_Menu();
+void cut_percent_TDF_data(vector<TDF>& data);
+void cut_quantity_TDF_data(vector<TDF>& data);
+void cut_time_TDF_data(vector<TDF>& data);
+void show_TDF_data(vector<TDF>& data);
+void pars_TDF_data_per_cid(vector<TDF>& data);
+void save_TDF_data_in_csv(vector<TDF>& data);
+void save_TDF_data_in_bin(vector<TDF>& data);
